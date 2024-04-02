52def1f897e08dbf1761a9651d5fb374c78df9b7
Sero <kmcaltiz@gmail.com>
2024-04-02 20:35:28 +0800

    implemented checking ng existing token2

--- app/src/main/java/com/mawd/swiftnotify/SignInActivity.java
+++ app/src/main/java/com/mawd/swiftnotify/SignInActivity.java
@@ -55,9 +55,7 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
-
         findReferences();
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
